a7b8eb0f4aae909d8345c3fa2245241caa1ec5ef
Алексей Шашин <alexshashin.2023@gmail.com>
2026-01-11 21:09:42 +0500

    Разработанные контексты для моделей. Шашин

---
+++ Classes/ClubsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pcClubs.Classes.Common;
+using pcClubs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcClubs.Classes
+{
+    public class ClubsContext : DbContext
+    {
+        public DbSet<Clubs> Clubs { get; set; }
+        public ClubsContext() => Database.EnsureCreated();
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcClubs.Classes.Common
+{
+    public class Config
+    {
+        public static string ConnectionConfig = "server=10.0.201.4;uid=root;pwd=;database=pcClub;";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
