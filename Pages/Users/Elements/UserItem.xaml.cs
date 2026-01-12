1bec56f3057d86c8a03d27dc28873909e459423e
alexeys6969 <alexshashin.2023@gmail.com>
2026-01-12 20:13:28 +0500

    Разработанный модуль пользователей. Фамилия

---
+++ Pages/Users/Elements/UserItem.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using pcClubs.Classes;
+
+namespace pcClubs.Pages.Users.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для UserItem.xaml
+    /// </summary>
+    public partial class UserItem : UserControl
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        Main Main;
+        Models.Users User;
+        public UserItem(Models.Users User, Main Main)
+        {
+            InitializeComponent();
+            this.Name.Text = User.FIO;
+            this.Date.Text = User.RentStart.ToString("yyyy-MM-dd");
+            this.Time.Text = User.RentStart.ToString("HH:mm");
+            this.Duration.Text = User.Duration.ToString();
+            this.Club.Text = AllClub.Clubs.Where(x => x.Id == User.IdClub).First().Name;
+            this.Main = Main;
+            this.User = User;
+        }
+
+        private void EditUser(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(new Pages.Users.Add(this.Main, this.User));
+        }
+
+        private void DeleteUser(object sender, RoutedEventArgs e)
+        {
+            Main.AllUsers.Users.Remove(User);
+            Main.AllUsers.SaveChanges();
+            Main.parent.Children.Remove(this);
+        }
+    }
+}
